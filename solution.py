b251422120fb0819b09824a71194703d3a138be6
anya-otman <73428194+anya-otman@users.noreply.github.com>
2024-05-25 23:29:20 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import ks_2samp
 
 
 chat_id = 1105842906
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.02
-    t_stat, p_value = ttest_ind(x, y, equal_var=False)
+    stat, p_value = ks_2samp(x, y)
 
     return p_value < alpha
